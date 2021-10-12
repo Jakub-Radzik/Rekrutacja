1843258dcbde5656fad9801adb8048c90ddbc213
Jakub Radzik <jradzik4@gmail.com>
2021-10-12 14:06:16 +0200

    structure

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ArticlesListingComponent} from "./articles-listing/articles-listing.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'articles', component: ArticlesListingComponent },
+  { path: 'articles/:id', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,23 @@ import {HttpClientModule} from "@angular/common/http";
 import {ArticleListingModule} from "./articles-listing/article-listing.module";
 import {CommonModule} from "./common/common.module";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingPageComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ArticleListingModule,
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
