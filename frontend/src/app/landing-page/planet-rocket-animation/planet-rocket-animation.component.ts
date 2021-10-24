a6fb60605b0f56eed8522bec0cf3c201d28820ab
Jakub Radzik <jradzik4@gmail.com>
2021-10-24 13:13:37 +0200

    landing page organization

--- frontend/src/app/landing-page/planet-rocket-animation/planet-rocket-animation.component.ts
+++ frontend/src/app/landing-page/planet-rocket-animation/planet-rocket-animation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-planet-rocket-animation',
   templateUrl: './planet-rocket-animation.component.html',
-  styleUrls: ['./planet-rocket-animation.component.css', './parallax.css']
+  styleUrls: ['./planet-rocket-animation.component.css', './parallax.css', './planet.css', './objects.css']
 })
 export class PlanetRocketAnimationComponent implements OnInit {
 
