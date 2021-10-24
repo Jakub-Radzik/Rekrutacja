f5d97ab5a149662b173623375cb46561be11c5e7
Jakub Radzik <jradzik4@gmail.com>
2021-10-24 22:35:10 +0200

    remove interceptors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {ArticleListingModule} from "./articles-listing/article-listing.module";
 import {CoreModule} from "./common/core.module";
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {AppRoutingModule} from './app-routing.module';
 import {CommonModule} from "@angular/common";
 import {SingleArticleModule} from "./single-article/single-article.module";
-import {BasicInterceptor} from "./common/utils/basic.interceptor";
 import {PageNotFoundModule} from "./page-not-found/page-not-found.module";
 import {LandingPageModule} from "./landing-page/landing-page.module";
 
@@ -29,8 +28,6 @@ import {LandingPageModule} from "./landing-page/landing-page.module";
     FontAwesomeModule,
     AppRoutingModule
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: BasicInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
