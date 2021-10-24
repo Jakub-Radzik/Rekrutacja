10432f538413485f7e267bb0713d9de6fa0bc5c1
Jakub Radzik <jradzik4@gmail.com>
2021-10-24 14:30:20 +0200

    death star easter egg

--- frontend/src/app/landing-page/landing-animation/landing-animation.component.ts
+++ frontend/src/app/landing-page/landing-animation/landing-animation.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-animation',
   templateUrl: './landing-animation.component.html',
-  styleUrls: ['./landing-animation.component.css', './landing-animation-planet.css', './landing-animation-objects.css', './landing-animation-parallax.css']
+  styleUrls: ['./landing-animation.component.css',
+    './landing-animation-planet.css',
+    './landing-animation-objects.css',
+    './parallax/landing-animation-parallax-stars.css',
+    './parallax/landing-animation-parallax-stars-1.css',
+    './parallax/landing-animation-parallax-stars-1.1.css',
+    './parallax/landing-animation-parallax-stars-2.css',
+    './parallax/landing-animation-parallax-stars-2.1.css',
+    './parallax/landing-animation-parallax-stars-3.css',
+    './parallax/landing-animation-parallax-stars-3.1.css'
+  ]
 })
 export class LandingAnimationComponent implements OnInit {
 
