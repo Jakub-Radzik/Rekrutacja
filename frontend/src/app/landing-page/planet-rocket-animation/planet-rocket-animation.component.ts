50c4392ef3a880d8c2cd5c8edc3088df25ee82d3
Jakub Radzik <jradzik4@gmail.com>
2021-10-24 12:25:42 +0200

    parallax

--- frontend/src/app/landing-page/planet-rocket-animation/planet-rocket-animation.component.ts
+++ frontend/src/app/landing-page/planet-rocket-animation/planet-rocket-animation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-planet-rocket-animation',
   templateUrl: './planet-rocket-animation.component.html',
-  styleUrls: ['./planet-rocket-animation.component.css']
+  styleUrls: ['./planet-rocket-animation.component.css', './parallax.css']
 })
 export class PlanetRocketAnimationComponent implements OnInit {
 
